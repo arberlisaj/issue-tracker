e491f9ce70de43ec3a31374f66756f8a556c8634
arberlisaj <arberlisaj1@gmail.com>
2024-03-04 18:07:30 +0100

    Add login and logout buttons

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Box } from "@radix-ui/themes";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
 
 function Navbar() {
   const currentPath = usePathname();
+  const { status, data: session } = useSession();
 
   const links = [
     { label: "Dashboard", href: "/" },
@@ -30,6 +33,14 @@ function Navbar() {
           </li>
         ))}
       </ul>
+      <Box>
+        {status === "authenticated" && (
+          <Link href="api/auth/signout">Logout</Link>
+        )}
+        {status === "unauthenticated" && (
+          <Link href="api/auth/signin">Login</Link>
+        )}
+      </Box>
     </nav>
   );
 }
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,19 @@
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@prisma/client";
 import NextAuth from "next-auth/next";
 import GithubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
+  session: {
+    strategy: "jwt",
+  },
 });
 
 export { handler as GET, handler as POST };
