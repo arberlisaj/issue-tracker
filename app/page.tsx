d2865567330d967f6c570d872896d70122c19d23
arberlisaj <arberlisaj1@gmail.com>
2024-03-05 17:00:52 +0100

    Implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
   return (
     <main>
-      <h1>hello world</h1>
+      <Pagination itemCount={100} pageSize={10} currentPage={2} />
     </main>
   );
 }
